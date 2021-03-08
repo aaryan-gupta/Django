f214dc77d55b3af53448a4ad407112d8440372e2
Aaryan Gupta <aaryangupta36@gmail.com>
2021-03-09 01:17:00 +0530

    10 homepage

--- textutils/textutils/views.py
+++ textutils/textutils/views.py
@@ -5,13 +5,18 @@ from django.shortcuts import render
 
 def index(request):
 	# return HttpResponse("<h1>HOME</h1>")
-	params = {"name": "Aaryan", "place": "Mars"}
-	return render(request, "index.html", params)
+	# params = {"name": "Aaryan", "place": "Mars"}
+	# return render(request, "index.html", params)
+	return render(request, "index.html")
 
 """ def about(request):
 	return HttpResponse("ABOUT") """
 
 def removepunc(request):
+	# GET THE TEXT
+	djtext = request.GET.get("text", "default")
+	print(djtext)
+	# ANALYZE THE TEXT
 	return HttpResponse("remove punc")
 
 def capfirst(request):
