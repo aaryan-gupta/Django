70486723a99625dd1b87dd808e208afcedf11524
Aaryan Gupta <aaryangupta36@gmail.com>
2021-03-09 00:50:47 +0530

    8 pipeline

--- textutils/textutils/views.py
+++ textutils/textutils/views.py
@@ -3,7 +3,22 @@
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("<h1>HELLO</h1>")
+	return HttpResponse("<h1>HOME</h1>")
 
-def about(request):
-	return HttpResponse("ABOUT")
+""" def about(request):
+	return HttpResponse("ABOUT") """
+
+def removepunc(request):
+	return HttpResponse("remove punc")
+
+def capfirst(request):
+	return HttpResponse("capitalize first")
+
+def newlineremove(request):
+	return HttpResponse("newline remove")
+
+def spaceremove(request):
+	return HttpResponse("space remover")
+
+def charcount(request):
+	return HttpResponse("charcount")
