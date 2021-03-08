14c836ec83a3cf4c481c1c7f1a7a35a3b32b2dc3
Aaryan Gupta <aaryangupta36@gmail.com>
2021-03-09 00:24:33 +0530

    6 views & urls

--- textutils/textutils/views.py
+++ textutils/textutils/views.py
@@ -1,1 +1,9 @@
 # I HAVE CREATED THIS FILE - AARYAN
+
+from django.http import HttpResponse
+
+def index(request):
+	return HttpResponse("<h1>HELLO</h1>")
+
+def about(request):
+	return HttpResponse("ABOUT")
