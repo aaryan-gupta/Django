f8c64168296e852c8d4179703d0b1489917b31ce
Aaryan Gupta <aaryangupta36@gmail.com>
2021-03-10 15:15:38 +0530

    32 template inheritance

--- MyAwesomeCart/mac/shop/views.py
+++ MyAwesomeCart/mac/shop/views.py
@@ -8,8 +8,8 @@ def index(request):
 	return render(request, "shop/index.html")
 
 def about(request):
-	return HttpResponse("About")
-	# return render(request, "shop/index.html")
+	# return HttpResponse("About")
+	return render(request, "shop/about.html")
 
 def contact(request):
 	return HttpResponse("Contact")
