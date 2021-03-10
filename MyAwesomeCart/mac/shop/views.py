83201eba2e213112139394ab969406633714b75f
Aaryan Gupta <aaryangupta36@gmail.com>
2021-03-10 11:20:43 +0530

    24 project layout

--- MyAwesomeCart/mac/shop/views.py
+++ MyAwesomeCart/mac/shop/views.py
@@ -6,3 +6,27 @@ from django.http import HttpResponse
 def index(request):
 	# return HttpResponse("Index Shop")
 	return render(request, "shop/index.html")
+
+def about(request):
+	return HttpResponse("About")
+	# return render(request, "shop/index.html")
+
+def contact(request):
+	return HttpResponse("Contact")
+	# return render(request, "shop/index.html")
+
+def tracker(request):
+	return HttpResponse("Tracker")
+	# return render(request, "shop/index.html")
+
+def search(request):
+	return HttpResponse("Search")
+	# return render(request, "shop/index.html")
+
+def productView(request):
+	return HttpResponse("Product View")
+	# return render(request, "shop/index.html")
+
+def checkout(request):
+	return HttpResponse("Checkout")
+	# return render(request, "shop/index.html")
