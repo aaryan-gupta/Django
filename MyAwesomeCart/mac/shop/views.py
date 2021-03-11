ddacaa879d110ff8ba928b03d0eefba266065a83
Aaryan Gupta <aaryangupta36@gmail.com>
2021-03-11 11:25:37 +0530

    41 adding cart popover & other pages

--- MyAwesomeCart/mac/shop/views.py
+++ MyAwesomeCart/mac/shop/views.py
@@ -25,25 +25,19 @@ def index(request):
 	return render(request, "shop/index.html", params)
 
 def about(request):
-	# return HttpResponse("About")
 	return render(request, "shop/about.html")
 
 def contact(request):
-	return HttpResponse("Contact")
-	# return render(request, "shop/index.html")
+	return render(request, "shop/contact.html")
 
 def tracker(request):
-	return HttpResponse("Tracker")
-	# return render(request, "shop/index.html")
+	return render(request, "shop/tracker.html")
 
 def search(request):
-	return HttpResponse("Search")
-	# return render(request, "shop/index.html")
+	return render(request, "shop/search.html")
 
 def productView(request):
-	return HttpResponse("Product View")
-	# return render(request, "shop/index.html")
+	return render(request, "shop/prodView.html")
 
 def checkout(request):
-	return HttpResponse("Checkout")
-	# return render(request, "shop/index.html")
+	return render(request, "shop/checkout.html")
