5135d93bb4570925e2522b5884ab92fb835bb94f
Aaryan Gupta <aaryangupta36@gmail.com>
2021-03-16 01:29:59 +0530

    80 msgs framework - showing msgs in blog

--- iCoder/home/views.py
+++ iCoder/home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse, redirect
+from django.contrib import messages
 from home.models import Contact
 
 # Create your views here.
@@ -8,15 +9,23 @@ def home(request):
 	return render(request, "home/home.html")
 
 def contact(request):
+	# messages.success(request, "Welcome to Contact")
+	# messages.error(request, "Welcome to Contact")
 	if request.method == "POST":
 		name = request.POST["name"]
 		email = request.POST["email"]
 		phone = request.POST["phone"]
 		content = request.POST["content"]
-		contact = Contact(name=name, email=email, phone=phone, content=content)
-		contact.save()
-		return redirect("/contact")
+		if len(name) < 2 or len(email) < 3 or len(phone) < 10 or len(content) < 4:
+			messages.error(request, "Please fill the form correctly.")
+		else:
+			contact = Contact(name=name, email=email, phone=phone, content=content)
+			contact.save()
+			messages.success(request, "Your message has been successfully sent.")
+			return redirect("/contact")
 	return render(request, "home/contact.html")
 
 def about(request):
+	""" messages.success(request, "This is About")
+	messages.error(request, "This is About") """
 	return render(request, "home/about.html")
