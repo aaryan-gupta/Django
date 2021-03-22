6b626bf06999543f3ff11fc112dd1029d611e737
Aaryan Gupta <66556617+aaryan-gupta@users.noreply.github.com>
2021-03-22 08:10:54 +0530

    88 validating signup form

--- iCoder/home/views.py
+++ iCoder/home/views.py
@@ -56,7 +56,17 @@ def handleSignup(request):
 		email = request.POST["email"]
 		pass1 = request.POST["pass1"]
 		pass2 = request.POST["pass2"]
+
 		# CHECK FOR ERRORNEOUS INPUTS
+		if len(username) > 10:
+			messages.error(request, "Username must be under 10 characters")
+			return redirect("/")
+		if not username.isalnum():
+			messages.error(request, "Username should only contain letters & numbers")
+		if pass1 != pass2:
+			messages.error(request, "Passwords do not match")
+			return redirect("/")
+		
 		# CREATE THE USER
 		myuser = User.objects.create_user(username, email, pass1)
 		myuser.first_name = fname
